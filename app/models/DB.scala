22630e5fb9ec624ae3f8dd2f409f5b5614f0e692
Antoine Detante <antoine.detante@intech.lu>
2012-11-18 17:44:30 +0100

    Upgrade DB config for trustedpaas support

--- app/models/DB.scala
+++ app/models/DB.scala
@@ -7,10 +7,17 @@ object DB {
 
   // Get a MongoDB connection based on application.conf
   lazy val connection =
-    MongoConnection(
-      configuration.getString("mongo.host").getOrElse("127.0.0.1"),
-      configuration.getInt("mongo.port").getOrElse(27017)
-      )(configuration.getString("mongo.dbname").getOrElse("race"))
+    configuration.getString("mongo.user").flatMap{username=>configuration.getString("mongo.password").flatMap{password=>Some(username,password)}}
+      .map{credentials=>
+        db:MongoDB=>
+          db.authenticate(credentials._1,credentials._2)
+          db
+      }.getOrElse(identity[MongoDB](_)).apply(
+        MongoConnection(
+          configuration.getString("mongo.host").getOrElse("127.0.0.1"),
+          configuration.getInt("mongo.port").getOrElse(27017)
+        )(configuration.getString("mongo.dbname").getOrElse("todos"))
+      )
 
 
 }
